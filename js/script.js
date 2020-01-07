2fd3a443d339bfae2dac0d7cbcf294e004eafa89
roma1984 <1512001143301Tt>
2020-01-07 09:12:09 +0300

    pravka24

---
+++ js/script.js
@@ -0,0 +1,67 @@
+ 
+    var link = document.querySelector(".btn-modal");
+    var popup = document.querySelector(".popup");
+
+    var close = popup.querySelector(".modal-close");
+
+    var form = popup.querySelector("form");
+
+    var login = popup.querySelector("[name=login]");
+
+    var password = popup.querySelector("[name=email]");
+
+    var isStorageSupport = true;
+  var storage = "";
+  
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+    
+    link.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("popup-show");
+
+      if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
+      login.focus();
+    }
+    
+      login.focus();
+
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("popup-show");
+    popup.classList.remove("popup-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value) {
+      evt.preventDefault();
+
+      popup.classList.remove("popup-error");
+      popup.offsetWidth = popup.offsetWidth; 
+
+      popup.classList.add("popup-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+      }
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("popup-show")) {
+        popup.classList.remove("popup-show");
+        popup.classList.remove("popup-error");
+      }
+    }
+  });
+ 
